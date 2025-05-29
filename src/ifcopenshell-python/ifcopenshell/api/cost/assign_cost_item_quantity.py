c4862fbdcd62d0fc862185ab43523dc4995456a4
falken10 <falken10vdl@gmail.com>
2025-05-29 23:40:02 +0200

    Update cost item quantity calculation to exclude construction resources

--- src/ifcopenshell-python/ifcopenshell/api/cost/assign_cost_item_quantity.py
+++ src/ifcopenshell-python/ifcopenshell/api/cost/assign_cost_item_quantity.py
@@ -154,5 +154,8 @@ class Usecase:
             if quantity.is_a("IfcQuantityCount"):
                 count = 0
                 for rel in self.settings["cost_item"].Controls:
-                    count += len(rel.RelatedObjects)
-                quantity[3] = count
+                    for obj in rel.RelatedObjects:
+                        # Only increment if not a resource
+                        if not obj.is_a("IfcConstructionResource"):
+                            count += 1
+                quantity[3] = count  # 3 is the QuantityValue attribute index for IfcQuantityCount
