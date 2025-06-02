0aca016ffcf5f874b60b1c1b12a966b0177fd882
falken10 <falken10vdl@gmail.com>
2025-06-02 09:28:35 +0200

    Restore wrong editing of add_information in document instead of library

--- src/ifcopenshell-python/ifcopenshell/api/document/add_information.py
+++ src/ifcopenshell-python/ifcopenshell/api/document/add_information.py
@@ -25,37 +25,58 @@ from typing import Optional
 def add_information(
     file: ifcopenshell.file, parent: Optional[ifcopenshell.entity_instance] = None
 ) -> ifcopenshell.entity_instance:
-    """Adds a new library information to the project"""
-    # Create appropriate attributes based on schema
-    if file.schema == "IFC2X3":
-        attributes = {"LibraryReference": "X", "Name": "UnnamedInformation"}
-    else:
-        attributes = {"Version": "1.0", "Name": "UnnamedInformation"}
-    
-    information = file.create_entity("IfcLibraryInformation", **attributes)
-    
+    """Adds a new document information to the project
+
+    An IFC document information is a document associated with the project.
+    It may be a drawing, specification, schedule, certificate, warranty
+    guarantee, manual, contract, and so on. They are often used for drawings
+    and facility management purposes.
+
+    A document may also be a subdocument of a larger document, this is
+    useful for superseding documents or tracking older versions. The parent
+    is considered the latest version and the children are older revisions.
+
+    :param parent: The parent document, if necessary.
+    :type parent: ifcopenshell.entity_instance, optional
+    :return: The newly created IfcDocumentInformation entity
+    :rtype: ifcopenshell.entity_instance
+
+    Example:
+
+    .. code:: python
+
+        document = ifcopenshell.api.document.add_information(model)
+        # A document typically has a unique drawing or document name (which
+        # follows a coding system depending on the project), as well as a
+        # title.  This should match what is shown on the titleblock or title
+        # page of the document. At a minimum you'd also want to specify a
+        # URI location. The location may be on local, or on a CDE, or any
+        # other platform.
+        ifcopenshell.api.document.edit_information(model,
+            information=document,
+            attributes={"Identification": "A-GA-6100", "Name": "Overall Plan",
+            "Location": "A-GA-6100 - Overall Plan.pdf"})
+    """
+    id_attribute = "DocumentId" if file.schema == "IFC2X3" else "Identification"
+    information = file.create_entity("IfcDocumentInformation", **{id_attribute: "X", "Name": "Unnamed"})
     if not parent and file.by_type("IfcProject"):
         parent = file.by_type("IfcProject")[0]
-        
-    if parent:
-        if parent.is_a("IfcProject") or parent.is_a("IfcContext"):
-            # Connect to project
+    if parent.is_a("IfcProject") or parent.is_a("IfcContext"):
+        file.create_entity(
+            "IfcRelAssociatesDocument",
+            GlobalId=ifcopenshell.guid.new(),
+            OwnerHistory=ifcopenshell.api.owner.create_owner_history(file),
+            RelatingDocument=information,
+            RelatedObjects=[parent],
+        )
+    elif parent.is_a("IfcDocumentInformation"):
+        if parent.IsPointer:
+            rel = parent.IsPointer[0]
+            documents = set(rel.RelatedDocuments)
+            documents.add(information)
+            rel.RelatedDocuments = list(documents)
+        else:
             file.create_entity(
-                "IfcRelAssociatesLibrary",
-                GlobalId=ifcopenshell.guid.new(),
-                OwnerHistory=ifcopenshell.api.owner.create_owner_history(file),
-                RelatingLibrary=information,
-                RelatedObjects=[parent],
+                "IfcDocumentInformationRelationship", RelatingDocument=parent, RelatedDocuments=[information]
             )
-        elif parent.is_a("IfcLibraryInformation"):
-            # For library to library nesting, create a relationship where
-            # information is RelatingLibrary and parent is in RelatedObjects
-            rel = file.create_entity(
-                "IfcRelAssociatesLibrary",
-                GlobalId=ifcopenshell.guid.new(),
-                OwnerHistory=ifcopenshell.api.owner.create_owner_history(file),
-                RelatingLibrary=information,
-                RelatedObjects=[parent],
-            )
-    
     return information
